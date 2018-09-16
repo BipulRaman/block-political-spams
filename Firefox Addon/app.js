b6aad47fd5f8a4fc9f4edc14fcc94e68e2137292
Bipul Raman <biraman@microsoft.com>
2018-09-16 10:28:32 +0530

    code updated

--- Firefox Addon/app.js
+++ Firefox Addon/app.js
@@ -9,11 +9,13 @@ function getFormattedDate() {
 }
 
 var xTime = getFormattedDate();
-var url = baseURL + "/data/blacklist.json?t=" + xTime;
+var url = baseURL + "/app/data/blacklist.json?t=" + xTime;
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         blacklist = JSON.parse(this.responseText);
-        readArray(blacklist);
+        Array.from(blacklist).forEach(function(eachItem){
+            console.log(eachItem);
+        });        
         setInterval(function () {
             removeSpamPost();
         }, 1000)
